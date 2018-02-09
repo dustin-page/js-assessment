16f7caee7ede36023ac9a4492fecae88f663ffa5
Dustin Page <dustinpage@live.com>
2018-02-09 15:29:37 -0600

    Add answers for functions.js

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,61 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+  argsAsArray: function (fn, arr) {
+
+    //Example 1: apply() method
+    //return fn.apply(null, arr);
+    //Example 2: Spread syntax
+    return fn(...arr);
+  },
+
+  speak: function (fn, obj) {
+    this.name = obj.name;
+    this.greeting = obj.greeting;
+    return fn.call(this);
+  },
+
+  functionFunction: function (str) {
+    return function insideFunc(str2) {
+      return str + ', ' + str2;
+    };
+  },
+
+  makeClosures: function (arr, fn) {
+
+    var functionsArr = [];
+
+    var closureFn = function (value) {
+      return function () { 
+        return fn(value) 
+      };
+    };
+
+    arr.forEach(function (value, index, array) {
+      functionsArr.push(closureFn(value));
+    });
+
+    return functionsArr;
 
   },
 
-  speak: function(fn, obj) {
+  partial: function (fn, str1, str2) {
 
   },
 
-  functionFunction: function(str) {
+  useArguments: function () {
 
   },
 
-  makeClosures: function(arr, fn) {
+  callIt: function (fn) {
 
   },
 
-  partial: function(fn, str1, str2) {
+  partialUsingArguments: function (fn) {
 
   },
 
-  useArguments: function() {
-
-  },
-
-  callIt: function(fn) {
-
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
+  curryIt: function (fn) {
 
   }
 };
