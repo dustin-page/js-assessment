49629e2f9a449505141eed435ca72a79a2a3de05
Dustin Page <dustinpage@live.com>
2018-02-09 10:37:32 -0600

    Add to async.js alternative way to retrieve data from server and return sorted array of names

--- app/async.js
+++ app/async.js
@@ -12,6 +12,8 @@ exports.asyncAnswers = {
   },
 
   manipulateRemoteData: function (url) {
+    //Example 1: Sort array of objects by name and then map the names to an array to return in the promise
+    /*
     var promise = jQuery.get(url).then(
       function (data) {
 
@@ -35,11 +37,33 @@ exports.asyncAnswers = {
         });
         return names;
       });
-
-
-
-    console.log(promise);
     return promise;
+    */
 
+    //Example 2: map the names to an array and then sort
+    var promise = jQuery.get(url).then(
+      function (data) {
+        var people = data.people;
+        var names = people.map(function (value, index, array) {
+          return value.name;
+        });
+
+        names.sort(function (a, b) {
+          var nameA = a.toUpperCase(); // ignore upper and lowercase
+          var nameB = b.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          // names must be equal
+          return 0;
+        });
+
+        return names;
+
+      });
+    return promise;
   }
 };
