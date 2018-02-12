93ff02b90e580ac9ce13cc597ccb2bca1a31ab3a
Dustin Page <dustinpage@live.com>
2018-02-12 11:39:37 -0600

    modules.js answer

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    return {
+      greeting: str1,
+      name: str2,
+      sayIt: function sayIt(){
+        return this.greeting + ", " + this.name;
+      }
+    }
   }
 };
